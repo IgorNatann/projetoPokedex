1bded4a0073f98d23ee052aa1a3bd95d9404c9e3
Igor Natan <igornatan4@gmail.com>
2022-03-13 18:14:44 -0300

    commit versão final projetoPokédex

--- src/js/index.js
+++ src/js/index.js
@@ -1,61 +1,24 @@
-/*
-------* Funcionalidades Necessárias *-------
-
-1 - Quando cliclar no pokemon da listagem, esconder o cartao do pokémon e mostrar o cartão correspondente ao que foi selecionado na listagem.
-
-2 - Para buscar os elementos dentro do document para verificar qual está sendo chamado, Utilizaremos dois elementos, 1-Listagem 2- Cartão do pokemon.
-
-3 - Armazenar em duas variáveis os elementos para poder trabalhar com os mesmo.
-
-4 - Precisaremos trabalhar com o evento de clique feito pelo úsuario na listagem de pokemons.
-
- ---------- Passos a fazer
-
-- remover a classe aberto só do cartão que estiver aberto.
-- ao clicar em um pokemon da listagem pegamos id desse pokémon para saer qual cartão mostrar.
-- remover a classe ativo que marca o pokémon selecionado.
-- adicionar a classe ativo no item da lista selecionado.
-
-- Manipular tirando classe e adicionando classe é o metódp mais rapido e eficaz.
-
-*/
-
-
-// querySelectorAll é  um metodo/função para buscar todos elementos da tela, sendo necessário setar quais elementos queremos buscar. Nesse caso estamos pegando todos elementos contidos dentro de Ul>Li , listagem dos pokemons!
-
-/* const para armazenar qual pokemon está sendo selecionado na listagem. */
+/* Criação Const */
 const listaSelecaoPokemons = document.querySelectorAll('.pokemon');
-
-/* const para armazenar qual cartao-pokemon esta aparecendo na tela */
 const pokemonsCard = document.querySelectorAll('.cartao-pokemon');
- 
-// forEach é um metado usado para iterar uma função, resume uma ação que possívelemte seria mt grande. Aplicando a mesma função para varíos elementos!
-// addEventListener é metodo//função usado para checar se uma função é chamada, e quando ser chamada executar algo.
+
+// Usando Function forEach para iterar elemento.
 listaSelecaoPokemons.forEach(pokemon => {
-    // Trabalhando com evento de clique feito pelo usuário na listagem pokemon.
-    pokemon.addEventListener('click', () => {
-      // QuerySelector é usado para fazer busca específica de apenas um elemento.
-      // fazendo busca do elemto que contem a class aberto  
-      const cartaoPokemonAberto = document.querySelector('.aberto') 
+  pokemon.addEventListener('click', () => {
+    const cartaoPokemonAberto = document.querySelector('.aberto') 
+    cartaoPokemonAberto.classList.remove('aberto')
 
-      //Removendo a class Aberto continda no elemento da Li. 
-      cartaoPokemonAberto.classList.remove('aberto')
+    const idPokemonSelecionado = pokemon.attributes.id.value
 
-      // ao clicar em um pokémon da listagem, Pegamos o Id do pokemon selecionado para saber qual cartão pokemon mostrar na tela
-      const idPokemonSelecionado = pokemon.attributes.id.value
+    const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
 
-      // Adicionando o Id do cartao pokemon que está selecionado.
-      const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
+    const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
+    cartaoPokemonParaAbrir.classList.add('aberto')
 
-      const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
-      cartaoPokemonParaAbrir.classList.add('aberto')
+    const pokemonAtivoNaListagem = document.querySelector('.ativo')
+    pokemonAtivoNaListagem.classList.remove('ativo')
 
-      // removendo a classe ativo do item anterior
-      const pokemonAtivoNaListagem = document.querySelector('.ativo')
-      pokemonAtivoNaListagem.classList.remove('ativo')
-
-      // adicionando a classe ativo ao pokemon da listagem selecionado.
-      const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
-      pokemonSelecionadoNaListagem.classList.add('ativo')
-    })
+    const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+    pokemonSelecionadoNaListagem.classList.add('ativo')
+  })
 })
