fcaa7a0a5e374528bc4dddbfd5528564b4517098
Igor Natan <igornatan4@gmail.com>
2022-03-13 17:57:32 -0300

    commit alteração index js

--- src/js/index.js
+++ src/js/index.js
@@ -9,53 +9,52 @@
 
 4 - Precisaremos trabalhar com o evento de clique feito pelo úsuario na listagem de pokemons.
 
- ----------
-- remover a classe aberto
+ ---------- Passos a fazer
+
+- remover a classe aberto só do cartão que estiver aberto.
+- ao clicar em um pokemon da listagem pegamos id desse pokémon para saer qual cartão mostrar.
+- remover a classe ativo que marca o pokémon selecionado.
+- adicionar a classe ativo no item da lista selecionado.
+
+- Manipular tirando classe e adicionando classe é o metódp mais rapido e eficaz.
 
 */
 
 
+// querySelectorAll é  um metodo/função para buscar todos elementos da tela, sendo necessário setar quais elementos queremos buscar. Nesse caso estamos pegando todos elementos contidos dentro de Ul>Li , listagem dos pokemons!
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// querySelectorAll é usado para setar quais os atributos queremos pegar dentro da div, nesse caso estamos pegando todos os pokemons que estão na listagem e cards
+/* const para armazenar qual pokemon está sendo selecionado na listagem. */
 const listaSelecaoPokemons = document.querySelectorAll('.pokemon');
 
+/* const para armazenar qual cartao-pokemon esta aparecendo na tela */
 const pokemonsCard = document.querySelectorAll('.cartao-pokemon');
  
 // forEach é um metado usado para iterar uma função, resume uma ação que possívelemte seria mt grande. Aplicando a mesma função para varíos elementos!
-
-// Trabalhando com evento de clique feito pelo usuário na listagem pokemon.
-// addEventListener é usado para checar se uma função é chamada, e quando ser chamada executar algo
+// addEventListener é metodo//função usado para checar se uma função é chamada, e quando ser chamada executar algo.
 listaSelecaoPokemons.forEach(pokemon => {
+    // Trabalhando com evento de clique feito pelo usuário na listagem pokemon.
     pokemon.addEventListener('click', () => {
-      
-      // fazendo busca de todos os elemtos que contem a class aberto  
+      // QuerySelector é usado para fazer busca específica de apenas um elemento.
+      // fazendo busca do elemto que contem a class aberto  
       const cartaoPokemonAberto = document.querySelector('.aberto') 
-      //Removendo do elemento que está com o seletor aberto. 
+
+      //Removendo a class Aberto continda no elemento da Li. 
       cartaoPokemonAberto.classList.remove('aberto')
 
+      // ao clicar em um pokémon da listagem, Pegamos o Id do pokemon selecionado para saber qual cartão pokemon mostrar na tela
       const idPokemonSelecionado = pokemon.attributes.id.value
 
+      // Adicionando o Id do cartao pokemon que está selecionado.
       const idDoCartaoPokemonParaAbrir = 'cartao-' + idPokemonSelecionado
 
       const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoPokemonParaAbrir)
       cartaoPokemonParaAbrir.classList.add('aberto')
 
+      // removendo a classe ativo do item anterior
       const pokemonAtivoNaListagem = document.querySelector('.ativo')
       pokemonAtivoNaListagem.classList.remove('ativo')
 
+      // adicionando a classe ativo ao pokemon da listagem selecionado.
       const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
       pokemonSelecionadoNaListagem.classList.add('ativo')
     })
